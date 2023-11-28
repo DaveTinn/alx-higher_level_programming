bc4e7fa96bb2ffa575739258a667d029b5999c0a
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2023-11-28 13:21:54 -0800

    A program printing the ASCII alphabet in lowercase omitting q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alphabets in range(97,123):
-    if (alphabets != 100 and alphabets != 101):
+    if (alphabets != 113 and alphabets != 101):
         print("{:c}".format(alphabets), end="")

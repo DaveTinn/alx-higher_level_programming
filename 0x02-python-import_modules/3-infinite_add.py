7ebbe6d59a6ecca4204cef474a0e3e99bee0eba0
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2023-11-30 07:38:23 -0800

    A program printing the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_argmt(argv):
+    num_of_elements = len(argv) - 1
+    if num_of_elements == 0:
+        print("{:d}".format(num_of_elements))
+    else:
+        index = 1
+        result = 0
+        while index <= num_of_elements:
+            result += int(argv[index])
+            index += 1
+        print("{:d}".format(result))
+
+if __name__ == "__main__":
+    import sys
+    add_argmt(sys.argv)

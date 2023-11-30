317a98f0d6d00a55ab36fae63cdb7cf583eccc7e
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2023-11-30 09:11:03 -0800

    A Python function that does exactly as a Python ByteCode

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from magic_calculation_102 import add, sub
+
+
+def magic_calculation(a, b):
+    if a < b:
+        c = add(a, b)
+        for index in range(1, 3):
+            c = add(c, index)
+        return (c)
+    else:
+        return sub(a, b)

abac09d16f5314221c4f5781120729ddc21919ab
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2023-12-19 12:05:19 -0800

    A class *Square* defining a square based on another Python file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Python Interpreter.'''
+
+
+class Square:
+    '''Defines a square.'''
+
+    def __init__(self, size):
+        '''Instantiating an instance of the class.
+
+        Variables:
+            size: size of the instance attribute.
+        '''
+        self.new_size = size

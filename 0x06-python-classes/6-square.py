6a2344df73a4e1c3b1b42b5d88c1132afd03623c
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2023-12-20 11:08:43 -0800

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,7 +29,7 @@ class Square:
         '''Property setter to set the size of instance attribute.'''
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
-        elif value < 0:
+        if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
 

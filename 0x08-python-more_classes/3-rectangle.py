d7843070e51d3a48eb350ac7dcfd72a2884d0713
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-01-05 04:08:01 -0800

    A String Representation of a class Rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -28,7 +28,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Sets the width of the Rectangle."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
@@ -42,7 +42,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Sets the height of the Rectangle."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')

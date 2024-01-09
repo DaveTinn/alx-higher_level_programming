f6819642e9e8a4f3a15849aa5615d9f6982d046e
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-01-09 05:07:03 -0800

    My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,7 +7,7 @@ class MyInt(int):
 
     def __new__(cls, *argmts, **k_argmts):
         """"Defines the new instance of the class."""
-        return super().__new__(cls, *argmts, **k_argmts)
+        return super(MyInt, cls).__new__(cls, *argmts, **k_argmts)
 
     def __eq__(self, invert):
         """Defines the equal == operator."""

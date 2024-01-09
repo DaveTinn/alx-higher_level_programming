df6e87ec0de951547ebd2b2b99b9154d97ed90d7
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-01-09 06:17:52 -0800

    A function returning the JSON representation of an object(string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python Interpreter."""
+
+
+import json
+"""The JSON module."""
+
+def to_json_string(my_obj):
+    """Defines the representation of an obj.
+
+    Arguments:
+        my_obj (str): The object to be represented.
+    """
+    return json.dumps(my_obj)

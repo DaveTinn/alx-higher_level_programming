1deee2f5e9e04125896dbc702e8f4f191552ed92
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-01-10 14:28:35 -0800

    Pascal's triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python Interpreter"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    p_triangle = []
+
+    for idx in range(n):
+        row = [1] * (idx + 1)
+
+        for i in range(1, idx):
+            row[i] = p_triangle[idx - 1][i - 1] + p_triangle[idx - 1][i]
+        p_triangle.append(row)
+
+    return p_triangle

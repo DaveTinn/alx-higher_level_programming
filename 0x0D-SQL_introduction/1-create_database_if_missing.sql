2ec0d2cf1873622bc5878463988996042c461c1d
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-02-13 13:11:00 -0800

    A script creating a database hbtn_0c_0 and should not fail if the database exists

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- A script that creates a database hbtn_0c_0
+-- If the database hbtn_0c_0 already exists, script should not fail
+-- SELECT or SHOW statements are forbidden
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;

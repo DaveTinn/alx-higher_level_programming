ba0f823bba81833ad763dc158fb75c6f194ab630
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-02-15 13:10:12 -0800

    Only Comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -9,6 +9,6 @@ FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.genre_id
 INNER JOIN tv_genres
-ON tv_show_genres.genre.id = tv_genres.id
+ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;

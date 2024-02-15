8fb1c4456d1426e25241e7d660f4158cd98ee6b5
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-02-14 16:50:01 -0800

    Lists all privileges of MySQL users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- A script that lists all privileges of the MySql users
+-- On my server (loaclhost)
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,2 @@
+A Readme on Structured Query Language(SQL) - More Queries > README.MD
+echo A Readme on Structured Query Language SQL - More Queries

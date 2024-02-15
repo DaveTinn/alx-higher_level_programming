a1faf6593eb02160369a56fe4261ce26208b7ec6
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-02-15 11:54:02 -0800

    Add Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,7 +5,7 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title, tv_shows_genres.genre_id
-FROM tv_shows
-INNER JOIN tv_shows_genres
-ON tv_shows.id = tv_shows_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres_genre_id ASC;
+	FROM tv_shows
+	INNER JOIN tv_shows_genres
+	ON tv_shows.id = tv_shows_genres.show_id
+	ORDER BY tv_shows.title, tv_show_genres_genre_id ASC;

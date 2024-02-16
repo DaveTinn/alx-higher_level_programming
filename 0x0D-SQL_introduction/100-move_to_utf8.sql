eadd5608c0d0a2c0f2b2e3254e36fb0dd3d70d2c
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-02-15 18:11:38 -0800

    Converts a database to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,6 +3,8 @@
 	-- The database "hbtn_0c_0"
 	-- The table "first_table"
 	-- Field "name" in "first_table"
-ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

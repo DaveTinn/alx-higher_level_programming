3ba05f28d5bcf7ecb44673bf5fccee99f289423c
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-02-15 17:57:13 -0800

    Displays average temperature by city

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- Import in hbtn_0c_0 database this table dump
+-- Displays the average temperature((Fahrenheit) by city ordered by temperature (descending)
+SELECT city, AVG(value) AS average_temperature
+FROM temperatures GROUP BY city
+ORDER BY average_temperature DESC;

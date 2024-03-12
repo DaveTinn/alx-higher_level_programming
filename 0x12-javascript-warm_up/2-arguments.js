466410ab01766123edafd9b62eb9d10674e6ef52
DaveTinn <ayodeji_oloruntoba@yahoo.com>
2024-03-12 01:21:18 +0000

    prints message depending on the number of arguments passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const numOfArgs = process.argv.length - 2;
+
+if (numOfArgs === 0)
+{
+	console.log('No Argument');
+}
+else if (numOfArgs === 1)
+{
+	console.log('Argument found');
+}
+else
+{
+	console.log('Arguments found');
+}

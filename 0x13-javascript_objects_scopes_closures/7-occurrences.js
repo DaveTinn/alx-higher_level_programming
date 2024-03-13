e1608ba019cdcbcb620ae9e1431e72a88cd24253
DaveTinn <ayodeji_oloruntoba@yahoo.com>
2024-03-13 22:49:27 +0000

    occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -4,7 +4,7 @@ exports.nbOccurences = function (list, searchElement) {
 
   list.reduce((result, element) => {
     numOfOccurence[element] = (numOfOccurence[element] || 0) + 1;
-  }, numOfOccurence);
+  }, {});
 
   return (numOfOccurence[searchElement] || 0);
 };

e1eb7daff4f233d67c4df1b3a1e613ac95d95e89
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-03-17 13:15:57 -0700

    Lists all State objects from the database

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A script listing all State objects
+from the databse hbtn_0e_6_usa,
+sorted in ascending order by states.id.
+"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    """Constructs the database URI."""
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                argv[1], argv[2], argv[3])
+
+    """Creates an SQLAlchemy engine."""
+    engine = create_engine(db_uri)
+
+    """Creates a factory Session."""
+    Session = sessionmaker(bind=engine)
+
+    """Creates a session."""
+    session = Session()
+
+    """Query all the State objects in order by states.id."""
+    states = session.query(State).order_by(State.id).limit(5).all()
+
+    # Prints the retrieved state objects
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))

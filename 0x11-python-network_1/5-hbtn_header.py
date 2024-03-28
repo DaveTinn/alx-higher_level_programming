925b2aeb6348ba2d5ff5eabe2bc1797065764a63
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-03-28 15:52:17 -0700

    Response header using the packages requests and sys

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,9 +6,9 @@ The packages requests and sys must be used.
 """
 
 
-from requests import get
+import requests
 from sys import argv
 
 if __name__ == "__main__":
-    r = get(argv[1])
+    r = requests.get(argv[1])
     print(r.headers.get('X-Request-Id'))

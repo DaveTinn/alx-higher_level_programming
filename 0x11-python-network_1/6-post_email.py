d787733d51da9fac9ca3603e2d826f1c8be530d4
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-03-28 16:11:17 -0700

    POST an email using the packages, requests and sys

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A script taking in a URL and an email,
+sends a POST request to the passed URL with the email as parameter,
+and displays the body of the response (decoded in utf-8).
+"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    payload = {'email': value}
+    r = requests.post(url, data=payload)
+    print(r.text)

6a8f35d475667e51187ef59cf3b6db3a334b51bf
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-03-28 15:30:31 -0700

    Error code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ from urllib import request
 with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
     print('Body response:')
-    print("\t - type: {}".format(type(html)))
-    print("\t - content: {}".format(html))
-    print("\t - utf8 content: {}".format(html.decode('utf-8')))
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))

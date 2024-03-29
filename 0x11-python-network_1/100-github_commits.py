2af524792ebf4ec071ac2c13f372b491f007e545
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-03-29 11:05:40 -0700

    Update on challenge

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         argv[2], argv[1]))
     commits = re.json()
     for commit in commits[:10]:
-        print(commit.get('shs'), end=': ')
+        print(commit.get('sha'), end=': ')
         print(commit.get('commit').get('author').get('name'))

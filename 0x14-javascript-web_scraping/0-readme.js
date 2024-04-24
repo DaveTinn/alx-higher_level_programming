d3cbd61df913d523f335044db1248735aaacc18b
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-04-24 16:56:50 -0700

    first commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const file_path = process.argv[2];
+const filepath = process.argv[2];
 const fs = require('fs');
-fs.readFile(file_path, 'utf8', function (err, data) {
+fs.readFile(filepath, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {

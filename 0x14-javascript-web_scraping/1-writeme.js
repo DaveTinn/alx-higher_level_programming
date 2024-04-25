31795cd6ce45563d4d1416e870230a2329c0fc0b
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-04-24 17:06:33 -0700

    writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const fs = require('fs');
+const filepath = process.argv[2];
+const string = process.argv[3];
+fs.writeFile(filepath, string, 'utf8', err => {
+  if (err) {
+    console.log(err);
+  }
+});
